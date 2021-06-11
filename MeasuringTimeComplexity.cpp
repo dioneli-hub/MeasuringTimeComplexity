52e2886ea8d0d16b9f884fe562080a353cfab02e
Dianka Levchenko <dianka_levchenko@inbox.ru>
2021-06-11 22:18:39 +0300

    Refactored the project

--- MeasuringTimeComplexity.cpp
+++ MeasuringTimeComplexity.cpp
@@ -8,32 +8,8 @@ using namespace std::chrono;
 
 const int MAX_RANDOM_VALUE = 1000;
 const int RANDOM_PRECISION_NUMBERS = 2;
-
-
-template <typename TFunc>
-nanoseconds time_of_execution(TFunc sort) {
-    auto start = high_resolution_clock::now();
-    sort();
-    auto stop = high_resolution_clock::now();
-
-    return duration_cast<nanoseconds>(stop - start);
-}
-
-void print_time_of_execution(nanoseconds total_time) {
-    // 1 second = 1 000 000 000 ns
-    cout << total_time.count() << "ns" << endl;
-}
-
-template <typename TElement>
-void print_array(TElement* arr, int size) {
-    cout << "[";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i];
-        if (i + 1 != size)
-            cout << ", ";
-    }
-    cout << "]" << endl;
-}
+const int ARRAY_SIZE = 50;
+const int COUNT_OF_TESTS = 5;
 
 template <typename TElement>
 TElement* generate_array(int size, TElement(*generate)()) {
@@ -44,6 +20,10 @@ TElement* generate_array(int size, TElement(*generate)()) {
     }
 
     return buffer;
+}
+
+int int_number_generator(int max) {
+    return rand() % max;
 }
 
 float float_number_generator() {
@@ -62,6 +42,11 @@ bool float_descending_comparer(float a, float b) {
 bool float_equal_comparer(float a, float b) {
     return a == b;
 }
+
+bool float_greather_than(float a, float b) {
+    return a > b;
+}
+
 
 template <typename TElement>
 void bubble_sort(TElement* arr, int sizeOfArray, bool(*comparer)(TElement, TElement)) {
@@ -195,20 +180,260 @@ void quick_sort(TElement* arr, int low, int high, bool(*comparer)(TElement, TEle
     }
 }
 
+template <typename TElement>
+int linear_search(TElement* arr, TElement target, int sizeOfArray, bool(*equal)(TElement, TElement)) {
+    for (int i = 0; i < sizeOfArray; i++) {
+        if (equal(arr[i], target)) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+template <typename TElement>
+int binary_search(TElement* arr, int len, TElement target, bool(*equal)(TElement, TElement),
+    bool(*greather_than)(TElement, TElement)) {
+    int max = (len - 1);
+    int min = 0;
+
+    int guess;
+    int step = 0;
+
+    while (max >= min) {
+        guess = (max + min) / 2;
+        step++;
+
+        if (equal(arr[guess], target)) {
+            return guess;
+        }
+        else {
+            if (greather_than(arr[guess], target)) {
+                max = (guess - 1);
+            }
+            else {
+                min = (guess + 1);
+            }
+        }
+    }
+
+    return -1;
+}
+
+void demo_linear_search() {
+    cout << "\t >>>>>> LINEAR SEARCH DEMO <<<<<<" << endl;
+    for (int i = 0; i < COUNT_OF_TESTS; i++) {
+        int sizeOfArray = (i + 1) * ARRAY_SIZE;
+        float* arr = generate_array<float>(sizeOfArray, float_number_generator);
+
+        int random_index = int_number_generator(sizeOfArray);
+
+        cout << endl
+            << "Linear search. Test #" << i + 1 << "." << endl
+            << "\t-size of array: " << sizeOfArray << endl
+            << "\t-random index: " << random_index << endl
+            << "\t-search value: " << arr[random_index] << endl;
+
+        steady_clock::time_point start = high_resolution_clock::now();
+        int search_index = linear_search<float>(arr, arr[random_index], sizeOfArray, float_equal_comparer);
+        steady_clock::time_point stop = high_resolution_clock::now();
+        nanoseconds execution_time = duration_cast<nanoseconds>(stop - start);
+
+        cout << "\t-linear search index: " << search_index << endl
+            << "\t-execution time: " << execution_time.count() << " nanoseconds" << endl
+            << endl;
+    }
+}
+
+void demo_binary_search() {
+    cout << "\t >>>>>> BINARY SEARCH DEMO <<<<<<" << endl;
+    for (int i = 0; i < COUNT_OF_TESTS; i++) {
+        int sizeOfArray = (i + 1) * ARRAY_SIZE;
+        float* arr = generate_array<float>(sizeOfArray, float_number_generator);
+
+        quick_sort<float>(arr, 0, sizeOfArray - 1, float_ascending_comparer, float_equal_comparer);
+
+        int random_index = int_number_generator(sizeOfArray);
+
+        cout << endl
+            << "Binary search. Test #" << i + 1 << "." << endl
+            << "\t-size of array: " << sizeOfArray << endl
+            << "\t-random index: " << random_index << endl
+            << "\t-search value: " << arr[random_index] << endl;
+
+        steady_clock::time_point start = high_resolution_clock::now();
+        int search_index = binary_search<float>(arr, sizeOfArray, arr[random_index], float_equal_comparer, float_greather_than);
+        steady_clock::time_point stop = high_resolution_clock::now();
+        nanoseconds execution_time = duration_cast<nanoseconds>(stop - start);
+
+        cout << "\t-binary search index: " << search_index << endl
+            << "\t-execution time: " << execution_time.count() << " nanoseconds" << endl
+            << endl;
+    }
+}
+
+void demo_bubble_sort() {
+    cout << "\t >>>>>> BUBBLE SORT DEMO <<<<<<" << endl;
+    for (int i = 0; i < COUNT_OF_TESTS; i++) {
+        int sizeOfArray = (i + 1) * ARRAY_SIZE;
+        float* arr = generate_array<float>(sizeOfArray, float_number_generator);
+
+        steady_clock::time_point random_start = high_resolution_clock::now();
+        bubble_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point random_stop = high_resolution_clock::now();
+
+        steady_clock::time_point sorted_start = high_resolution_clock::now();
+        bubble_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point sorted_stop = high_resolution_clock::now();
+
+        bubble_sort<float>(arr, sizeOfArray, float_descending_comparer);
+        steady_clock::time_point reversed_start = high_resolution_clock::now();
+        bubble_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point reversed_stop = high_resolution_clock::now();
+
+
+        cout << endl
+            << "Bubble sort. Test #" << i + 1 << "." << endl
+            << "\t-size of array: " << sizeOfArray << endl
+            << "\t-radom array sort time: " << duration_cast<nanoseconds>(random_stop - random_start).count() << " nanoseconds" << endl
+            << "\t-sorted array sort time: " << duration_cast<nanoseconds>(sorted_stop - sorted_start).count() << " nanoseconds" << endl
+            << "\t-reversed array sort time: " << duration_cast<nanoseconds>(reversed_stop - reversed_start).count() << " nanoseconds" << endl
+            << endl;
+    }
+}
+
+void demo_insertion_sort() {
+    cout << "\t >>>>>> INSERTION SORT DEMO <<<<<<" << endl;
+    for (int i = 0; i < COUNT_OF_TESTS; i++) {
+        int sizeOfArray = (i + 1) * ARRAY_SIZE;
+        float* arr = generate_array<float>(sizeOfArray, float_number_generator);
+
+        steady_clock::time_point random_start = high_resolution_clock::now();
+        insertion_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point random_stop = high_resolution_clock::now();
+
+        steady_clock::time_point sorted_start = high_resolution_clock::now();
+        insertion_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point sorted_stop = high_resolution_clock::now();
+
+        insertion_sort<float>(arr, sizeOfArray, float_descending_comparer);
+        steady_clock::time_point reversed_start = high_resolution_clock::now();
+        insertion_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point reversed_stop = high_resolution_clock::now();
+
+
+        cout << endl
+            << "Insertion sort. Test #" << i + 1 << "." << endl
+            << "\t-size of array: " << sizeOfArray << endl
+            << "\t-radom array sort time: " << duration_cast<nanoseconds>(random_stop - random_start).count() << " nanoseconds" << endl
+            << "\t-sorted array sort time: " << duration_cast<nanoseconds>(sorted_stop - sorted_start).count() << " nanoseconds" << endl
+            << "\t-reversed array sort time: " << duration_cast<nanoseconds>(reversed_stop - reversed_start).count() << " nanoseconds" << endl
+            << endl;
+    }
+}
+
+void demo_selection_sort() {
+    cout << "\t >>>>>> SELECTION SORT DEMO <<<<<<" << endl;
+    for (int i = 0; i < COUNT_OF_TESTS; i++) {
+        int sizeOfArray = (i + 1) * ARRAY_SIZE;
+        float* arr = generate_array<float>(sizeOfArray, float_number_generator);
+
+        steady_clock::time_point random_start = high_resolution_clock::now();
+        selection_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point random_stop = high_resolution_clock::now();
+
+        steady_clock::time_point sorted_start = high_resolution_clock::now();
+        selection_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point sorted_stop = high_resolution_clock::now();
+
+        selection_sort<float>(arr, sizeOfArray, float_descending_comparer);
+        steady_clock::time_point reversed_start = high_resolution_clock::now();
+        selection_sort<float>(arr, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point reversed_stop = high_resolution_clock::now();
+
+
+        cout << endl
+            << "Selection sort. Test #" << i + 1 << "." << endl
+            << "\t-size of array: " << sizeOfArray << endl
+            << "\t-radom array sort time: " << duration_cast<nanoseconds>(random_stop - random_start).count() << " nanoseconds" << endl
+            << "\t-sorted array sort time: " << duration_cast<nanoseconds>(sorted_stop - sorted_start).count() << " nanoseconds" << endl
+            << "\t-reversed array sort time: " << duration_cast<nanoseconds>(reversed_stop - reversed_start).count() << " nanoseconds" << endl
+            << endl;
+    }
+}
+
+void demo_quick_sort() {
+    cout << "\t >>>>>> QUICK SORT DEMO <<<<<<" << endl;
+    for (int i = 0; i < COUNT_OF_TESTS; i++) {
+        int sizeOfArray = (i + 1) * ARRAY_SIZE;
+        float* arr = generate_array<float>(sizeOfArray, float_number_generator);
+
+        steady_clock::time_point random_start = high_resolution_clock::now();
+        quick_sort<float>(arr, 0, sizeOfArray - 1, float_ascending_comparer, float_equal_comparer);
+        steady_clock::time_point random_stop = high_resolution_clock::now();
+
+        steady_clock::time_point sorted_start = high_resolution_clock::now();
+        quick_sort<float>(arr, 0, sizeOfArray - 1, float_ascending_comparer, float_equal_comparer);
+        steady_clock::time_point sorted_stop = high_resolution_clock::now();
+
+        quick_sort<float>(arr, 0, sizeOfArray - 1, float_descending_comparer, float_equal_comparer);
+        steady_clock::time_point reversed_start = high_resolution_clock::now();
+        quick_sort<float>(arr, 0, sizeOfArray - 1, float_ascending_comparer, float_equal_comparer);
+        steady_clock::time_point reversed_stop = high_resolution_clock::now();
+
+
+        cout << endl
+            << "Selection sort. Test #" << i + 1 << "." << endl
+            << "\t-size of array: " << sizeOfArray << endl
+            << "\t-radom array sort time: " << duration_cast<nanoseconds>(random_stop - random_start).count() << " nanoseconds" << endl
+            << "\t-sorted array sort time: " << duration_cast<nanoseconds>(sorted_stop - sorted_start).count() << " nanoseconds" << endl
+            << "\t-reversed array sort time: " << duration_cast<nanoseconds>(reversed_stop - reversed_start).count() << " nanoseconds" << endl
+            << endl;
+    }
+}
+
+void demo_merge_sort() {
+    cout << "\t >>>>>> MERGE SORT DEMO <<<<<<" << endl;
+    for (int i = 0; i < COUNT_OF_TESTS; i++) {
+        int sizeOfArray = (i + 1) * ARRAY_SIZE;
+        float* arr = generate_array<float>(sizeOfArray, float_number_generator);
+
+        steady_clock::time_point random_start = high_resolution_clock::now();
+        merge_sort<float>(arr, 0, sizeOfArray - 1, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point random_stop = high_resolution_clock::now();
+
+        steady_clock::time_point sorted_start = high_resolution_clock::now();
+        merge_sort<float>(arr, 0, sizeOfArray - 1, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point sorted_stop = high_resolution_clock::now();
+
+        merge_sort<float>(arr, 0, sizeOfArray - 1, sizeOfArray, float_descending_comparer);
+        steady_clock::time_point reversed_start = high_resolution_clock::now();
+        merge_sort<float>(arr, 0, sizeOfArray - 1, sizeOfArray, float_ascending_comparer);
+        steady_clock::time_point reversed_stop = high_resolution_clock::now();
+
+
+        cout << endl
+            << "Selection sort. Test #" << i + 1 << "." << endl
+            << "\t-size of array: " << sizeOfArray << endl
+            << "\t-radom array sort time: " << duration_cast<nanoseconds>(random_stop - random_start).count() << " nanoseconds" << endl
+            << "\t-sorted array sort time: " << duration_cast<nanoseconds>(sorted_stop - sorted_start).count() << " nanoseconds" << endl
+            << "\t-reversed array sort time: " << duration_cast<nanoseconds>(reversed_stop - reversed_start).count() << " nanoseconds" << endl
+            << endl;
+    }
+}
+
 int main()
 {
-    int size = 15;
-    float* arr = generate_array<float>(size, float_number_generator);
 
-    print_array(arr, size);
-
-    // merge_sort(arr, 0, size - 1, size, float_ascending_comparer);
-
-    quick_sort<float>(arr, 0, size - 1, float_descending_comparer, float_equal_comparer);
-
-    print_array(arr, size);
-
+    demo_linear_search();
+    demo_binary_search();
+    demo_bubble_sort();
+    demo_insertion_sort();
+    demo_selection_sort();
+    demo_quick_sort();
+    demo_merge_sort();
 
     return 0;
 }
+
 
